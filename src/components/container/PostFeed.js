326f09c300e3aabbeb2afd256c63911c5100f9e6
Leo Cheung <lappangcheung@gmail.com>
2020-03-15 14:07:00 -0400

    adding the flat list

--- src/components/container/PostFeed.js
+++ src/components/container/PostFeed.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import { FlatList } from 'react-native';
 import {Post} from '../presentation';
 
 class PostFeed extends Component {
+
+    _renderPost = () => {
+        return <Post />
+    }
+
+    _returnKey = (item) => {
+        return item.toString()
+    }
+
     render() {
         return (
-            <Post />
+            <FlatList 
+                data={[1,2,3,4,5,6,7,8,9]}
+                keyExtractor={this._returnKey}
+                renderItem={this._renderPost}
+            />
         );
     };
 };
